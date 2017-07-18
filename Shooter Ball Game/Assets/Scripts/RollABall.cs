cff30ef749665f9911b52b0a81ffe2931b5a8a30
ConnorPhalen <phalen.connor@hotmail.co.uk>
2017-07-17 20:33:36 -0700

    Added a basic scene with a controllable rolling ball.

---
+++ Shooter Ball Game/Assets/Scripts/RollABall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollABall : MonoBehaviour
+{
+
+    // public declarations
+    public float speed;
+
+    // prvate declarations
+    private Rigidbody rb;
+    private Vector3 movementForceVector;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        movementForceVector = new Vector3();
+    }
+
+    // Update is called once per frame, Before rendering
+    void Update()
+    {
+
+    }
+
+    // Every Frame, Called before Physics Calculations
+    private void FixedUpdate()
+    {
+        // Get input from player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // put input into a Vector3
+        movementForceVector.x = moveHorizontal;
+        movementForceVector.y = 0.0f;
+        movementForceVector.z = moveVertical;
+
+        // Apply vector force to player
+        rb.AddForce(movementForceVector * speed);
+    }
+}
