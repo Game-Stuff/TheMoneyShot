4d031a132341e1bd7d59f16416c01a486f922c70
ConnorPhalen <phalen.connor@hotmail.co.uk>
2017-07-19 17:15:43 -0700

    Added a new scene where you have an item stuck to your camera's rotation and placement. You can also unleash your ball of fury with the left click. Simple Physics.

---
+++ Shooter Ball Game/Assets/Scripts/FPSBallSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FPSBallSpawner : MonoBehaviour {
+
+    public GameObject parentWeapon;
+    public GameObject ballBullet;
+
+    [SerializeField] private Rigidbody ballRigidBody;
+    [SerializeField] private bool      m_isShooting;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ballRigidBody = ballBullet.GetComponent<Rigidbody>(); ;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // is player pressing the Shoot key
+        m_isShooting = Input.GetButton("Fire1");
+
+        // if the player has clicked the fire key and the ball is stopped
+        if (m_isShooting && ballRigidBody.velocity.magnitude < 0.1f)
+        {
+            ballRigidBody.velocity.Set(0.0f, 0.0f, 0.0f);  // Might not have to do as it should be stopped already
+            ballBullet.transform.position = this.transform.position;
+            ballBullet.transform.rotation = parentWeapon.transform.rotation;
+
+            ballRigidBody.AddRelativeForce(0.0f, 800.0f, 0.0f);
+        }
+    }
+}
